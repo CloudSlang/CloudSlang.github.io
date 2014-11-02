29c5aa06efb11749940f8d27d9e7405871cac3de
dorsha <doron@dorsha.com>
2014-11-02 15:15:00 +0200

    Removed yeoman content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,21 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name scoreWebsiteApp
- * @description
- * # scoreWebsiteApp
- *
- * Main module of the application.
- */
 angular
-  .module('scoreWebsiteApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('scoreWebsiteApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name scoreWebsiteApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the scoreWebsiteApp
- */
 angular.module('scoreWebsiteApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+
+    });

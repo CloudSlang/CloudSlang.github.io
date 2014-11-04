2643315847b9e81fd76fc51c7839104975b100b5
dorsha <doron@dorsha.com>
2014-11-04 20:23:31 +0200

    Fixed images issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,6 +193,7 @@ module.exports = function (grunt) {
                     '<%= yeoman.dist %>/scripts/{,*/}*.js',
                     '<%= yeoman.dist %>/styles/{,*/}*.css',
                     '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                    '!<%= yeoman.dist %>/images/team/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
                     '<%= yeoman.dist %>/styles/fonts/*'
                 ]
             }
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -3,32 +3,37 @@
 angular.module('scoreWebsiteApp')
     .controller('AboutCtrl', function () {
 
-    }).directive('hoverMemberImage', function(){
+    }).directive('hoverMemberImage', ['$timeout', function(timer) {
         return {
             restrict: 'E',
             replace: true,
             template:
                 '<div>' +
-                    '<img class="img-responsive" ng-src="{{memberImage}}" height="225px" width="308px">' +
+                    '<img class="img-responsive" src="{{memberImage}}" height="225px" width="308px">' +
                     '<p class="h4 text-center">{{memberName}}</p>' +
                 '</div>',
             scope: {
                 memberImage: '=member',
                 memberName: '@name'
             },
-            link: function(scope, elm, attrs){
-                var imagesPath = 'images/team/';
-                var hoverPrefox = 'yearbook';
-                scope.memberName = attrs.name;
-                scope.memberImage = imagesPath + hoverPrefox + attrs.member + ".jpg";
-                elm.bind('mouseenter',function(){
-                    scope.memberImage = imagesPath + attrs.member + ".jpg";
-                    scope.$apply();
-                });
-                elm.bind('mouseleave',function(){
-                    scope.memberImage = imagesPath + hoverPrefox + attrs.member + ".jpg";
-                    scope.$apply();
-                });
+            link: function(scope, elm, attrs) {
+
+                var hoverMemberImage = function () {
+                    var imagesPath = 'images/team/';
+                    var hoverPrefix = 'yearbook';
+                    scope.memberName = attrs.name;
+                    scope.memberImage = imagesPath + hoverPrefix + attrs.member + ".jpg";
+                    elm.bind('mouseenter',function(){
+                        scope.memberImage = imagesPath + attrs.member + ".jpg";
+                        scope.$apply();
+                    });
+                    elm.bind('mouseleave',function(){
+                        scope.memberImage = imagesPath + hoverPrefix + attrs.member + ".jpg";
+                        scope.$apply();
+                    });
+                };
+
+                timer(hoverMemberImage, 0); // must be renered after dom is finished
             }
         };
-    });
+    }]);

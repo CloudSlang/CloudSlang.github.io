c3676a52b07431c2f175741e9ae208aaaad23838
dorsha <doron@dorsha.com>
2014-11-12 10:02:03 +0200

    Changed to read more on wiki

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,7 +31,7 @@ angular.module('scoreWebsiteApp')
             mainBulletsProcessBasedDesc: 'The score language (slang) is an intuitive language to describe workflows in a re-usable fashion that can run using both the score library or the score CLI.',
             mainBulletsMultiLingualDesc: 'Designed to support multiple compilers that can generate low level execution instructions, score serves as an orchestration platform for potentially any process language out there.',
             mainBulletsLightweightDesc: 'Small footprint, ootb content and an easy-to-embed approach makes score the perfect choice as the  workflow orchestration solution in your application.',
-            mainBulletsReadMore: 'Read More',
+            mainBulletsReadMore: 'Read More on Wiki',
 
             // contact us
             contactUsHeader: 'You\'d like to reach out to us? Drop us a message!',

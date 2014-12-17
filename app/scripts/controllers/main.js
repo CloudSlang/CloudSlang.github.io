9a77b9129c495dde692a2f05dbfc2d22bdd7fb1f
Eliya Sadan <eliyasadan@gmail.com>
2014-12-17 16:28:47 +0200

    added movies responsiv and stuff...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,23 @@ angular.module('scoreWebsiteApp')
             { id: 'about', title: $rootScope.messages.navAboutTitle },
             { id: 'documentation', title: $rootScope.messages.navDocumentationTitle  }
         ];
-    }).directive('loadingPage', [function() {
-    return {
-        restrict: 'E',
-        template: '<div class="loading-page"></div>',
-        link: function() {
-            Pace.on("done", function() {
-                $('div.loading-page').fadeOut('slow');
-            });
-        }
-    };
-}]);
+    }).directive('loadingPage', [function () {
+        return {
+            restrict: 'E',
+            template: '<div class="loading-page"></div>',
+            link: function () {
+                Pace.on("done", function () {
+                    $('div.loading-page').fadeOut('slow');
+                });
+            }
+        };
+    }]).directive('youtubeEmbed', [function () {
+        return {
+            restrict: 'A',
+            template: '',
+            link: function (scope, element) {
+                var src = $(element).find('img')[0].src;
+                optimizeYouTubeEmbeds(src);
+            }
+        };
+    }]);

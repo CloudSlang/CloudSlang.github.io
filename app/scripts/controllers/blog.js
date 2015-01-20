1b9a04c203e4fd0fb3fca8037e1c26a8a52f012e
dorsha <doron@dorsha.com>
2015-01-20 18:48:53 +0200

    Redirect blog instead of iframe

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('scoreWebsiteApp')
-    .controller('BlogCtrl', function ($rootScope, MessagesService) {
-        _.forEach($('.navbar-collapse'), function(target) {
-            $(target).collapse({'toggle': false});
-        });
-
-        $rootScope.sections = [];
-        $rootScope.navSwitch = { uri: '', title: $rootScope.messages.navBackToSiteTitle };
+    .controller('BlogCtrl', function () {
+        window.location.replace('http://blog.openscore.io');
+        //_.forEach($('.navbar-collapse'), function(target) {
+        //    $(target).collapse({'toggle': false});
+        //});
+        //
+        //$rootScope.sections = [];
+        //$rootScope.navSwitch = { uri: '', title: $rootScope.messages.navBackToSiteTitle };
     });

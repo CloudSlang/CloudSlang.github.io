5da93a8f1cde1b13dc8e2cc13577f68cf9bb1161
dorsha <doron@dorsha.com>
2015-04-18 00:38:48 +0300

    Added github cards to about us

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: '0.0.0.0',
+                hostname: '127.0.0.1',
                 livereload: 35729
             },
             livereload: {

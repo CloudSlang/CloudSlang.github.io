2a9f143b6139d2e201074dcf5fc001cecaefa37a
unknown <malachig@MALACHIG1.emea.hpqcorp.net>
2015-07-12 10:35:30 +0300

    enable  404.html

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-/*app.get('/status',function(req, res){
+app.get('/status',function(req, res){
     res.send('/status GET OK');
-});*/
+});
 
 
-/*app.use(function(req, res) {
+app.use(function(req, res) {
     var err = new Error('Not Found');
     err.status = 404;
-    res.sendFile(__dirname + '/404.html')
-});*/
+    res.sendFile(__dirname + '/public/404.html')
+});
 
 
 app.set('port', (process.env.PORT || 5000));

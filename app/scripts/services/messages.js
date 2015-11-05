587683cf0605a04d64eec222f34c89b05264e71f
Gal Malachi <gal.malachi@hpe.com>
2015-11-05 15:33:18 +0200

    privacy and terms of use update

--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -94,7 +94,7 @@ angular.module('cloudSlangWebsiteApp')
             contactPlaceholderMessage: 'Your Message',
 
             // about us
-            aboutUsByHp: 'Project CloudSlang by HP',
+            aboutUsByHp: 'Project CloudSlang by HPE',
             aboutUsWhoWeAreText: 'This project is being contributed to the open source community by HP Software engineers with the goal of leveraging the power of community to create the best-in-class orchestration technology.',
 
             // get started banner
